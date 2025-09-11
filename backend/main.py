a30257d2aedea375e880ca04bc3626ed26d716b7
billysams21 <bllysm23@gmail.com>
2025-09-11 11:27:58 +0700

    build conf

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ from app.main import app
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "main:app",
+        "app.main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000))
+        port=int(os.getenv("PORT", 8080))
         # reload=True
     )
